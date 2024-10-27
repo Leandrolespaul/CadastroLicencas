8ca758ef987dc458249d2642990b0a6dfc7ab70b
leandrolespaul <leandrolespaul01@hotmail.com>
2024-10-27 14:34:52 -0300

    Salvei com Datas

--- teste.js
+++ teste.js
@@ -1,8 +1,20 @@
-import { format } from "date-fns"
-import { addDays } from "date-fns";
-import ptBR from 'date-fns/locale/pt-BR';
-import parse from 'date-fns/parse';
+import { format, parseJSON } from "date-fns"
+import { addDays } from "date-fns"
+import ptBR from 'date-fns/locale/pt-BR'
+import parse from 'date-fns/parse'
 import { differenceInDays } from "date-fns"
+import { intervalToDuration } from "date-fns"
+
+// let cont = 1
+// const id = () => cont++
+
+// const funcionarios = []
+
+// const cadastroFunc = (nome, dataNascimento, CPF, telefone, cidade, CEP, logradouro, bairro, numero, matricula, localTrabalho, dataAdmissao) => {
+//     funcionarios.push({ id: id(), nome, dataNascimento, CPF, telefone, cidade, CEP, logradouro, bairro, numero, matricula, localTrabalho, dataAdmissao })
+// }
+
+
 
 const dataLicenca = (dataUsuario, dias) => {
     let dataEntrada = parse(dataUsuario, 'dd/MM/yyyy', new Date(), { locale: ptBR })
@@ -16,12 +28,37 @@ const distanciaEntreDatas = (dataInicial, dataFinal) => {
     let dataSaida = parse(dataFinal, 'dd/MM/yyyy', new Date(), { locale: ptBR })
     const diasEntreDatas = differenceInDays(dataSaida, dataEntrada)
     console.log(`Entre a data ${dataInicial} e ${dataFinal} o servidor tem ${diasEntreDatas} dias.`)
-   
+
 }
+
+const intervaloDataAdmissao = (dataAdmissao, dataSaida) => {
+    let dataEntrada = parse(dataAdmissao, 'dd/MM/yyyy', new Date(), { locale: ptBR })
+    if (dataSaida){
+        let dataFinal  = parse(dataSaida, 'dd/MM/yyyy', new Date(), { locale: ptBR })
+        const intervalo = intervalToDuration({
+            start: new Date(dataEntrada),
+            end: new Date(dataFinal)
+        })
+        if (intervalo.days === undefined) intervalo.days = 0
+        if (intervalo.months === undefined) intervalo.months = 0
+        if (intervalo.years === undefined) intervalo.years = 0
+        console.log(`Seu tempo de trabalho é de ${intervalo.days} dia(s) ${intervalo.months} mês(s) e ${intervalo.years} ano(s).`)  
+    }else {
+        const intervalo = intervalToDuration({
+            start: new Date(dataEntrada),
+            end: new Date()
+        }); 
+    }
+}
+
+
+
+
 
 dataLicenca("26/10/2024", 30)
 console.log('_________________________________')
-distanciaEntreDatas("27/10/2024", "30/11/2024")
-
+distanciaEntreDatas("27/10/2024", "03/03/2025")
+console.log('_________________________________')
+intervaloDataAdmissao("27/10/2024", "03/03/2025")
 
 
