f7c3dff90e99dd61d54e1ea029adc03f990983a6
leandrolespaul <leandrolespaul01@hotmail.com>
2024-10-27 11:33:40 -0300

    atualizado

--- teste.js
+++ teste.js
@@ -2,20 +2,26 @@ import { format } from "date-fns"
 import { addDays } from "date-fns";
 import ptBR from 'date-fns/locale/pt-BR';
 import parse from 'date-fns/parse';
+import { differenceInDays } from "date-fns"
 
-
-const dataPtBr = (dataUsuario, dias) => {
-let dataEntrada = parse(dataUsuario, 'dd/MM/yyyy', new Date(), { locale: ptBR })  
-const  calculo = addDays(dataEntrada, dias) 
-const data = new Date(calculo)
-
-const dataFormatada = data.toLocaleDateString("pt-BR", {
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric"
-})
-
-console.log(dataFormatada)
+const dataLicenca = (dataUsuario, dias) => {
+    let dataEntrada = parse(dataUsuario, 'dd/MM/yyyy', new Date(), { locale: ptBR })
+    const calculo = addDays(dataEntrada, dias)
+    const dataFormatada = format(new Date(calculo), "dd/MM/yyyy")
+    console.log(`Sua Liceça termina em ${dataFormatada}`)
 }
 
-dataPtBr("26/10/2024", 30)
+const distanciaEntreDatas = (dataInicial, dataFinal) => {
+    let dataEntrada = parse(dataInicial, 'dd/MM/yyyy', new Date(), { locale: ptBR })
+    let dataSaida = parse(dataFinal, 'dd/MM/yyyy', new Date(), { locale: ptBR })
+    const diasEntreDatas = differenceInDays(dataSaida, dataEntrada)
+    console.log(`Entre a data ${dataInicial} e ${dataFinal} o servidor tem ${diasEntreDatas} dias.`)
+   
+}
+
+dataLicenca("26/10/2024", 30)
+console.log('_________________________________')
+distanciaEntreDatas("27/10/2024", "30/11/2024")
+
+
+
